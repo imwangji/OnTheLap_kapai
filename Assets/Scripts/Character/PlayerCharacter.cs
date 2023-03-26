cb6e25566307d9088e29deeca6df0cd54ca61e78
imwangji <86795863@qq.com>
2023-03-26 22:05:06 +0800

    人物等级成长部分

--- Assets/Scripts/Character/PlayerCharacter.cs
+++ Assets/Scripts/Character/PlayerCharacter.cs
@@ -3,8 +3,34 @@ using System.Collections.Generic;
 using KapaiGame;
 using UnityEngine;
 
-public class PlayerCharacter :Character
+namespace KapaiGame
 {
-    int Mana;
-    public List<SkillCard> SkillCards;
+    public class PlayerCharacter :Character
+    {
+        private int _baseHealth = 50;
+        private int _baseMana = 20;
+        private int _baseSpeed = 5;
+        private int initExp = 0;
+        private int Level = 1;
+        int Mana = 0;
+        public List<SkillCard> SkillCards = new List<SkillCard>();
+
+        public PlayerCharacter()
+        {
+        
+        }
+
+        void LevelUp()
+        {
+            Level++;
+            UpdateStatus();
+        }
+        void UpdateStatus()
+        {
+            Health = (int)(_baseHealth * Mathf.Pow(1.15f, Level - 1));
+            Mana = (int)(_baseHealth * Mathf.Pow(1.05f, Level - 1));
+            Speed = (int)(_baseHealth * Mathf.Pow(1.001f, Level - 1));
+        }
+    }
 }
+
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using KapaiGame;
 using UnityEngine;
 
 namespace Manager
